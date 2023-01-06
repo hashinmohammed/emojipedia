957b61be7cb07a4aafd495ee81a2a751ecc86e1f
hashinmohammed <hashinpks@gmail.com>
2023-01-06 12:08:30 +0530

    starting-project

---
+++ src/emojipedia.js
@@ -0,0 +1,45 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },{
+    id: 4,
+    emoji: "😖",
+    name: " Confounded Face",
+    meaning:
+      "How on earth could that happen? Smiley is extremely dismayed and bewildered. Or damn angry: That's almost the final straw!"
+  },
+  {
+    id: 5,
+    emoji: "🥶",
+    name: " Freezing face",
+    meaning:
+      "It is so cold, you are about to freeze to death. The smile is almost frozen. Already totally blue in the face from the cold temperature, tortured facial expression. Only a cup of tea, coffee, or the hot tub can help now."
+  },
+  {
+    id: 6,
+    emoji: "😱",
+    name: " Face Screaming in Fear",
+    meaning:
+      "Horror-stricken face with two hands, screaming. Is scared stiff, feeling nothing but fear and panic. The creepy emoji can also be used ironically or as a symbol for Halloween."
+  }
+  
+];
+export default emojipedia;
